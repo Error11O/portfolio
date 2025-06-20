6ba08d2e9baa448e2bc1623e18cf63e0247bf0f2
error11O <matty.egan2112@gmail.com>
2025-06-20 23:38:11 +0100

    5th times the charm

--- site.js
+++ site.js
@@ -1,19 +1,15 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import serverless from 'serverless-http';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
 
 const app = express();
 
-const templatesDir = path.join(__dirname, 'templates');
+const templatesDir = path.join(__dirname, '..', 'templates');
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '..')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(templatesDir, 'home.html'));
 });
 
-export const handler = serverless(app);
+module.exports.handler = serverless(app);
